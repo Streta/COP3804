187a9ef83a59bc87c8fff1dcc584c7b03fc51f8d
Roosevelt Mendieta <Rmend106@fiu.edu>
2019-06-06 15:43:52 -0400

    ZIP added

--- Program2/Main.java
+++ Program2/Main.java
@@ -1,3 +1,16 @@
+/*********************************************************************
+ * Author : Roosevelt Mendieta Course: Java - 2 Professor: Michael Robinson
+ * Program #: 2
+ * 
+ * Due Date : 06/12/2019
+ * 
+ * 
+ * Certification: I hereby certify that this work is my own and none of it is
+ * the work of any other person.
+ * 
+ * ..........{ Roosevelt Mendieta }..........
+ *********************************************************************/
+
 public class Main {
 
     public static void main(String[] args) {
